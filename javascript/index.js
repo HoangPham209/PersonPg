727a1b4e31827210dc18d239bbcf04fec4193901
Hoang Pham <59801146+HoangPham209@users.noreply.github.com>
2022-04-19 15:57:26 -0400

    Huge updates
    Add mult. pages and also make it look better. Add anims and also assets adding

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,31 @@
-let btn = document.querySelector(".btn");
-let item = document.querySelector(".box");
+// let btn = document.querySelector(".btn");
+// let item = document.querySelector(".box");
 
-document.onload = () => {
-    item.classList.toggle('animate');
-    item.addEventListener('animationend', animationEndCallback);
-};
+// document.onload = () => {
+//     item.classList.toggle('animate');
+//     item.addEventListener('animationend', animationEndCallback);
+// };
 
-let animationEndCallback = (e) => {
-    item.removeEventListener('animationend', animationEndCallback);
-    item.classList.remove('animate');
-}
+// let animationEndCallback = (e) => {
+//     item.removeEventListener('animationend', animationEndCallback);
+//     item.classList.remove('animate');
+// }
+
+// animation for sliding and scroll
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 50;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
